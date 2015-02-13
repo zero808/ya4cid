e9b5f5aee5749aee56d176ba01f125044ae18d78
Duarte Barreira <duartemtbarreira@gmail.com>
2015-02-13 16:37:47 +0000

    Followed an anon's advice and changed some style issues and also corrected a bug where there would be an error in case it was the first time the user downloaded images from a given board.

--- ya4cid.rkt
+++ ya4cid.rkt
@@ -3,14 +3,13 @@
 (require json racket/port net/url yotsubAPI)
 
 ;Default paths
-(define 4chan-folder (build-path (find-system-path 'home-dir) "4chan"))
+(define current-4chan-folder (make-parameter (build-path (find-system-path 'home-dir) "4chan")))
 
 (define image-url-prefix "http://i.4cdn.org/g/")
 
-;make sure the folder exists
-(define (create-4chan-dir)
-  (cond ((not (directory-exists? 4chan-folder))
-         (make-directory 4chan-folder))))
+(define (create-dir-unless-exists dir)
+  (unless (directory-exists? dir)
+    (make-directory dir)))
 
 (define (get-filenames ht)
   ;TODO get some options to filter undesirable extensions, e.g., download only webm files
@@ -29,18 +28,17 @@
                          (lambda (out) (write-bytes (download url) out)) #:exists 'truncate))
 
 (define (download-images-from-thread board id)
-  (let ([thread (4chan-data-thread board (string->number id))] ;get the thread's data
-        [board-path (build-path 4chan-folder board)]
-        [new-thread-path (build-path 4chan-folder board id)])
-    (cond ((not (directory-exists? board-path))
-           (make-directory board-path))
-          ((not (directory-exists? new-thread-path))
-           (make-directory new-thread-path)))
-    (map (lambda (file-name) (save-image (string-append image-url-prefix file-name) new-thread-path file-name))
-         (get-filenames thread))
-    (displayln (format "Files saved in ~a" (path->string new-thread-path)))))
+  (define thread (4chan-data-thread board (string->number id))) ;get the thread's data
+  (define board-path (build-path (current-4chan-folder) board))
+  (define new-thread-path (build-path (current-4chan-folder) board id))
+  (create-dir-unless-exists board-path)
+  (create-dir-unless-exists new-thread-path)
+  (for-each (lambda (file-name)
+              (save-image (string-append image-url-prefix file-name) new-thread-path file-name))
+            (get-filenames thread))
+  (printf "Files saved in ~a~%" (path->string new-thread-path)))
 
-(create-4chan-dir)
+(create-dir-unless-exists (current-4chan-folder))
 ;TODO cycle for all the pairs of <board thread>
 (let ([arguments (current-command-line-arguments)])
   (download-images-from-thread (vector-ref arguments 0) (vector-ref arguments 1)))
